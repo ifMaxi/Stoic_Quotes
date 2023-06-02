0f48e6c0dba8892bdd0a9df2e64d9df220ab239e
Maxi Pelizzoni <maxi96p@gmail.com>
2023-06-01 22:54:06 -0300

    Type

--- app/src/main/java/com/maxidev/stoicquotes/ui/theme/Type.kt
+++ app/src/main/java/com/maxidev/stoicquotes/ui/theme/Type.kt
@@ -2,18 +2,43 @@ package com.maxidev.stoicquotes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.maxidev.stoicquotes.R
+
+val archivoFamily = FontFamily(
+    Font(R.font.archivo, FontWeight.Normal)
+)
+
+val jostFamily = FontFamily(
+    Font(R.font.jost, FontWeight.Normal),
+    Font(R.font.jost_medium, FontWeight.Medium)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = jostFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 34.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = archivoFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = jostFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 25.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
